6c35c8bbbc4fec89ffa4016c8550994bf2eeeb73
peterzhang <zhh520@gmail.com>
2018-06-08 01:52:06 +0200

    [FIX] Fix 'Connection was refused' error

--- lilspider/middlewares/proxy.py
+++ lilspider/middlewares/proxy.py
@@ -6,6 +6,8 @@ class ProxyMiddleware(object):
     """Inject proxy settings for HttpProxyMiddleware"""
 
     def process_request(self, request: scrapy.http.Request, spider: scrapy.spiders.Spider) -> None:
+        if not spider.settings.getbool('HTTPPROXY_ENABLED', False):
+            return
         _proxy = spider.settings.get('HTTPPROXY_ADDRESS')
         if type(_proxy) is str and re.match(r'^http', _proxy):
             request.meta['proxy'] = _proxy
