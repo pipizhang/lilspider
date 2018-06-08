b077cbdbfffd07df97aaa90e29635959d3c73754
peterzhang <zhh520@gmail.com>
2018-06-08 02:02:08 +0200

    [UPDATE] update agents

--- lilspider/agents.py
+++ lilspider/agents.py
@@ -2,8 +2,10 @@
 from typing import List
 
 AGENTS_ALL = [
-    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0'
+    'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'
     ] # type: List[str]
 AGENTS = [
-    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0'
+    'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'
     ] # type: List[str]
