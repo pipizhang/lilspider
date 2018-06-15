b5751f8efa7750cad3c6d80d5fcc26c607db4ddd
peterzhang <zhh520@gmail.com>
2018-06-15 16:41:56 +0200

    [ADD] Add jieba text rank

--- lilspider/utils/zh.py
+++ lilspider/utils/zh.py
@@ -7,4 +7,8 @@ def extract_tags(content: str, topK: int=5) -> List[str]:
     words = jieba.analyse.extract_tags(content, topK=topK, allowPOS=allow)
     return words
 
+def textrank(content: str, topK: int=10) -> List:
+    allow = ('ns', 'n', 'vn')
+    rank = jieba.analyse.textrank(data, topK=topK, withWeight=True, allowPOS=allow)
+    return rank
 
